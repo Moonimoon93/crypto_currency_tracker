1a76b0eeac928fb51dc8ed9e7792119b016cce6c
Hyun Seung Moon <harrymoon93@Hyunui-MacBookAir.local>
2022-05-26 16:09:19 +0900

    :lipstick: Finished Basic styling setup 1. Added styled.d.ts file in order to use theme.ts. 2. Removed all of default styling by adding createGlobalStyle from styled-components. 3. Added a font family, Oswald from Google Fonts. 4. Now theme has 3 props inside, bgColor, textColor, and accentColor.
    Happy Coding!

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,7 +1,15 @@
+import styled from "styled-components";
+
 function Coins() {
+  const Title = styled.h1`
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+    font-size: 2rem;
+  `;
+
   return (
     <div>
-      <h1>Coins</h1>
+      <Title> List of Coins</Title>
     </div>
   );
 }
